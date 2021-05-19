041f1352ca297eb7b7a2cc363ed2e5e3879b9f7f
romanovich-k <81236887+romanovich-k@users.noreply.github.com>
2021-05-19 13:36:12 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,99 @@
+
+//Slider
+$(document).ready(function(){
+	$('.slider__content').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    infinite: false,
+    autoplaySpeed: 3000,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
+		responsive:[
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+		]
+	});
+});
+
+
+
+// Меню бургер
+$(document).ready(function() {
+  $('.nav__icon').click(function(event) {
+    $('.nav__icon,.header__info').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+
+$('.nav__body').click(function(){
+    $('.nav__icon, .header__info').removeClass('active');
+    $('body').removeClass('lock');
+});
+
+
+
+
+//медленный скролл
+// Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+// Цикл по всем ссылкам
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
+
+//button to top
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if($(window).scrollTop()>300){
+    $('.scroll-top').fadeIn(250);
+}
+else{
+    $('.scroll-top').fadeOut(250);
+}
+  });
+  $('.scroll-top').click(function(){
+$('html,body').animate(
+    {scrollTop:0},400
+);
+  });
+});
+
+
+
+
+
+
+
+
+
